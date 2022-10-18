f5bf69fce020797d7fd8d8559ad15172ef15b29a
Simeon113 <Simeon113@byui.edu>
2022-10-17 18:16:52 -0600

    Week 5 updates

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -5,11 +5,28 @@ function toggleMenu() {
 const x = document.getElementById("menu");
 x.onclick = toggleMenu;
 
-document.getElementById("date").innerHTML = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(new Date());
+let date = new Date()
 
-document.getElementById("year").innerHTML = new Date().getFullYear()
+document.getElementById("date").innerHTML = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(date);
+
+document.getElementById("year").innerHTML = date.getFullYear()
 
 document.getElementById('modified').textContent = document.lastModified
 
+if (date.getDay() == 1 || date.getDay() == 2) {
+    document.getElementById("meeting").style.display = 'block'
+}
+function switchimage() {
+    if (window.innerWidth >= 1040) {
+        document.getElementById('img').src =  "images/herolarge.png"
+    }
+    else if (window.innerWidth >= 560) {
+        document.getElementById('img').src =  "images/heromedium.png"
+    }
+    else {
+        document.getElementById('img').src =  "images/herosmall.png"
+    }
+}
+switchimage()
 
-
+window.addEventListener('resize', switchimage)
