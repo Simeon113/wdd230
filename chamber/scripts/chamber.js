d1ef028925b000edb5fe4274412293eb4de533c8
Simeon113 <Simeon113@byui.edu>
2022-10-26 01:34:46 -0600

    fixed html & js errors

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -14,19 +14,5 @@ document.getElementById("year").innerHTML = date.getFullYear()
 document.getElementById('modified').textContent = document.lastModified
 
 if (date.getDay() == 1 || date.getDay() == 2) {
-    document.getElementById("meeting").style.display = 'block'
-}
-// function switchimage() {
-//     if (window.innerWidth >= 1040) {
-//         document.getElementById('img').src =  "images/herolarge.png"
-//     }
-//     else if (window.innerWidth >= 560) {
-//         document.getElementById('img').src =  "images/heromedium.png"
-//     }
-//     else {
-//         document.getElementById('img').src =  "images/herosmall.png"
-//     }
-// }
-// switchimage()
+    document.getElementById("meeting").style.display = 'block'}
 
-window.addEventListener('resize', switchimage)
