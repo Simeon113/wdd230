8859b39be8b17fa6dbcb3e7ae788d9cbd1fd04a5
Simeon113 <Simeon113@byui.edu>
2022-10-26 01:08:41 -0600

    finally

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -16,17 +16,17 @@ document.getElementById('modified').textContent = document.lastModified
 if (date.getDay() == 1 || date.getDay() == 2) {
     document.getElementById("meeting").style.display = 'block'
 }
-function switchimage() {
-    if (window.innerWidth >= 1040) {
-        document.getElementById('img').src =  "images/herolarge.png"
-    }
-    else if (window.innerWidth >= 560) {
-        document.getElementById('img').src =  "images/heromedium.png"
-    }
-    else {
-        document.getElementById('img').src =  "images/herosmall.png"
-    }
-}
-switchimage()
+// function switchimage() {
+//     if (window.innerWidth >= 1040) {
+//         document.getElementById('img').src =  "images/herolarge.png"
+//     }
+//     else if (window.innerWidth >= 560) {
+//         document.getElementById('img').src =  "images/heromedium.png"
+//     }
+//     else {
+//         document.getElementById('img').src =  "images/herosmall.png"
+//     }
+// }
+// switchimage()
 
 window.addEventListener('resize', switchimage)
