21b6c32a9b6e4b1cc116df53ecbec1c483f63d2d
Simeon113 <Simeon113@byui.edu>
2022-11-01 00:58:25 -0600

    again

--- key_animations_video/key_animations.js
+++ key_animations_video/key_animations.js
@@ -1,6 +1,6 @@
 const sample = document.querySelector("div")
 // sample.addEventListener("animationend", appear)
-sample.addEventListener("animationstart", appear)
+// sample.addEventListener("animationstart", appear)
 
 function appear() {
     document.querySelector("#example2").style.display = "block"
