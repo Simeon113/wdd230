08dca93b21d05aad8c8fcf3996882a7d48ce3648
Simeon113 <Simeon113@byui.edu>
2022-11-01 00:51:23 -0600

    made fixes

--- key_animations_video/key_animations.js
+++ key_animations_video/key_animations.js
@@ -1,9 +1,9 @@
-const sample = document.querySelector(div)
-sample.addEventListener("animationend", appear)
-// sample.addEventListener("animationstart", appear)
+const sample = document.querySelector("div")
+// sample.addEventListener("animationend", appear)
+sample.addEventListener("animationstart", appear)
 
 function appear() {
-    document.querySelector("#example").getElementsByClassName.display = "block"
+    document.querySelector("#example2").style.display = "block"
 }
 
 sample.className = "sample"
