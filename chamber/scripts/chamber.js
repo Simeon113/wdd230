589288d7c4fbf4714bc151908d58d6c0511fa227
Simeon113 <Simeon113@byui.edu>
2022-10-31 23:44:00 -0600

    finished again

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -16,3 +16,38 @@ document.getElementById('modified').textContent = document.lastModified
 if (date.getDay() == 1 || date.getDay() == 2) {
     document.getElementById("meeting").style.display = 'block'}
 
+
+    let imagesToLoad = document.querySelectorAll("img[data-src]");
+    const loadImages = (image) => {
+      image.setAttribute("src", image.getAttribute("data-src"));
+      image.onload = () => {
+        image.removeAttribute("data-src");
+      };
+    };
+    
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+      });
+    
+    // if ("IntersectionObserver" in window) {
+    //     const observer = new IntersectionObserver((items, observer) => {
+    //       items.forEach((item) => {
+    //         if (item.isIntersecting) {
+    //           loadImages(item.target);
+    //           observer.unobserve(item.target);
+    //         }
+    //       });
+    //     });
+    //     imagesToLoad.forEach((img) => {
+    //       observer.observe(img);
+    //     });
+    //   } else {
+    //     imagesToLoad.forEach((img) => {
+    //       loadImages(img);
+    //     });
+    //   }
+
+    let visits = Number(window.localStorage.getItem('visits-ls'))
+    visits++
+    localStorage.setItem('visits-ls',visits)
+    document.querySelector('#visits').innerText = visits
