bfdacbe2717c2c219234e7f10e69ed9b7034a6fb
Simeon113 <Simeon113@byui.edu>
2022-11-01 00:21:15 -0600

    added javascropt

---
+++ key_animations_video/key_animations.js
@@ -0,0 +1,9 @@
+const sample = document.querySelector(div)
+sample.addEventListener("animationend", appear)
+// sample.addEventListener("animationstart", appear)
+
+function appear() {
+    document.querySelector("#example").getElementsByClassName.display = "block"
+}
+
+sample.className = "sample"
