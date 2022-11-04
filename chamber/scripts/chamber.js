f22f670e70d5f6f472996cfa03f97b9e4a0506af
Simeon113 <Simeon113@byui.edu>
2022-11-04 03:30:34 -0600

    finished!

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -17,17 +17,17 @@ if (date.getDay() == 1 || date.getDay() == 2) {
     document.getElementById("meeting").style.display = 'block'}
 
 
-    let imagesToLoad = document.querySelectorAll("img[data-src]");
-    const loadImages = (image) => {
-      image.setAttribute("src", image.getAttribute("data-src"));
-      image.onload = () => {
-        image.removeAttribute("data-src");
-      };
-    };
+    // let imagesToLoad = document.querySelectorAll("img[data-src]");
+    // const loadImages = (image) => {
+    //   image.setAttribute("src", image.getAttribute("data-src"));
+    //   image.onload = () => {
+    //     image.removeAttribute("data-src");
+    //   };
+    // };
     
-    imagesToLoad.forEach((img) => {
-        loadImages(img);
-      });
+    // imagesToLoad.forEach((img) => {
+    //     loadImages(img);
+    //   });
     
     // if ("IntersectionObserver" in window) {
     //     const observer = new IntersectionObserver((items, observer) => {
@@ -47,7 +47,33 @@ if (date.getDay() == 1 || date.getDay() == 2) {
     //     });
     //   }
 
-    let visits = Number(window.localStorage.getItem('visits-ls'))
-    visits++
-    localStorage.setItem('visits-ls',visits)
-    document.querySelector('#visits').innerText = visits
+    // let visits = Number(window.localStorage.getItem('visits-ls'))
+    // visits++
+    // localStorage.setItem('visits-ls',visits)
+    // document.querySelector('#visits').innerText = visits
+
+
+
+    let thisvisit = date.getTime()
+
+  
+    let lastvisit = window.localStorage.getItem('last_visit')
+    let date_difference = thisvisit - lastvisit
+    let date_difference_days = (Math.round(date_difference / 1000 / 60 / 60 / 24))
+    localStorage.setItem('last_visit', thisvisit)
+
+    if (date_difference > 0) {
+      document.querySelector('#visits').innerText = `You last visited this site ${date_difference_days} days ago`
+    }
+    else {
+      document.querySelector('#visits').innerText = "This is your first time to this site"
+    }
+
+    window.addEventListener('load', getLoadDate)
+
+    function getLoadDate() {
+      let loadDate = new Date
+      // return loadDate
+    }
+
+    document.querySelector('#loadDate').value = loadDate
