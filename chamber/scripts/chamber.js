6199115c7c6633a350f229bab8d1017b16145da6
Simeon113 <Simeon113@byui.edu>
2022-11-11 16:42:52 -0700

    added json

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -75,3 +75,43 @@ function getLoadDate() {
 
 document.querySelector('#loadDate').value = loadDate
 
+const requestURL = 'https://simeon113.github.io/wdd230/chamber/data.json';
+const cards = document.querySelector('.cards');
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject);
+        const buisnesses = jsonObject['buisnesses'];
+        prophets.forEach(displayBuisnesses);
+    })
+
+function displayBuisnesses(buisness) {
+    let logo = document.createElement('img')
+    let h2 = document.createElement('h2');
+    let link = document.createElement('link');
+
+    logo.setAttribute('src', buisness.imageurl);
+    logo.setAttribute('alt', `${buisness.name} Logo`);
+    logo.setAttribute('loading', 'lazy');
+
+    h2.innerText = `${buisness.address} 
+    ${buisness.phone}`;
+
+    link.innerText = buisness.website
+
+
+
+
+
+    card.appendChild(logo);
+    card.appendChild(h2);
+    card.appendChild(link)
+
+
+
+    document.querySelector('div.cards').appendChild(card);
+}
+
