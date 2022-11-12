05874354e5e9c354519f4f673a709ed6e5ba8483
Simeon113 <Simeon113@byui.edu>
2022-11-11 17:12:21 -0700

    link test

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,79 +1,79 @@
-const images = document.querySelectorAll("[data-src]")
+// const images = document.querySelectorAll("[data-src]")
 
-function preloadImage(img) {
-  const src = img.getAttribute("data-src");
-  if(!src) {
-    return;
-  }
+// function preloadImage(img) {
+//   const src = img.getAttribute("data-src");
+//   if(!src) {
+//     return;
+//   }
 
-  img.src = src;
-  img.removeAttribute("data-src");
-}
-
-
-const imgOptions = {
-  threshold: 1
-};
-
-const imgObserver = new IntersectionObserver((entries, imgObserver) => {
-  entries.forEach(entry => {
-      if (!entry.isIntersecting) {
-        return;
-      } else {
-        preloadImage(entry.target);
-        imgObserver.unobserve(entry.target)
-      }
-  })
-}, imgOptions)
-
-images.forEach(image => {
-  imgObserver.observe(image);
-})
-
-function toggleMenu() {
-    document.getElementById("navigation").classList.toggle("open");
-    document.getElementById("menu").classList.toggle("open");
-}
-const x = document.getElementById("menu");
-x.onclick = toggleMenu;
-
-let date = new Date()
-
-document.getElementById("date").innerHTML = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(date);
-
-document.getElementById("year").innerHTML = date.getFullYear()
-
-document.getElementById('modified').textContent = document.lastModified
-
-if (date.getDay() == 1 || date.getDay() == 2) {
-    document.getElementById("meeting").style.display = 'block'}
+//   img.src = src;
+//   img.removeAttribute("data-src");
+// }
 
 
+// const imgOptions = {
+//   threshold: 1
+// };
+
+// const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+//   entries.forEach(entry => {
+//       if (!entry.isIntersecting) {
+//         return;
+//       } else {
+//         preloadImage(entry.target);
+//         imgObserver.unobserve(entry.target)
+//       }
+//   })
+// }, imgOptions)
+
+// images.forEach(image => {
+//   imgObserver.observe(image);
+// })
+
+// function toggleMenu() {
+//     document.getElementById("navigation").classList.toggle("open");
+//     document.getElementById("menu").classList.toggle("open");
+// }
+// const x = document.getElementById("menu");
+// x.onclick = toggleMenu;
+
+// let date = new Date()
+
+// document.getElementById("date").innerHTML = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(date);
+
+// document.getElementById("year").innerHTML = date.getFullYear()
+
+// document.getElementById('modified').textContent = document.lastModified
+
+// if (date.getDay() == 1 || date.getDay() == 2) {
+//     document.getElementById("meeting").style.display = 'block'}
 
 
-let thisvisit = date.getTime()
+
+
+// let thisvisit = date.getTime()
 
   
-let lastvisit = window.localStorage.getItem('last_visit')
-let date_difference = thisvisit - lastvisit
-let date_difference_days = (Math.round(date_difference / 1000 / 60 / 60 / 24))
-localStorage.setItem('last_visit', thisvisit)
+// let lastvisit = window.localStorage.getItem('last_visit')
+// let date_difference = thisvisit - lastvisit
+// let date_difference_days = (Math.round(date_difference / 1000 / 60 / 60 / 24))
+// localStorage.setItem('last_visit', thisvisit)
 
-if (date_difference > 0) {
-  document.querySelector('#visits').innerText = `You last visited this site ${date_difference_days} days ago`
-}
-else {
-  document.querySelector('#visits').innerText = "This is your first time to this site"
-}
+// if (date_difference > 0) {
+//   document.querySelector('#visits').innerText = `You last visited this site ${date_difference_days} days ago`
+// }
+// else {
+//   document.querySelector('#visits').innerText = "This is your first time to this site"
+// }
 
-window.addEventListener('load', getLoadDate)
+// window.addEventListener('load', getLoadDate)
 
-function getLoadDate() {
-  let loadDate = new Date
-  return loadDate
-}
+// function getLoadDate() {
+//   let loadDate = new Date
+//   return loadDate
+// }
 
-document.querySelector('#loadDate').value = loadDate
+// document.querySelector('#loadDate').value = loadDate
 
 const requestURL = 'https://simeon113.github.io/wdd230/chamber/data/data.json';
 const cards = document.querySelector('.cards');
@@ -92,7 +92,7 @@ function displayBuisnesses(buisness) {
     let card = document.createElement('section');
     let logo = document.createElement('img')
     let h2 = document.createElement('h2');
-    let link = document.createElement('link');
+    let link = document.createElement('a');
 
     logo.setAttribute('src', buisness.imageurl);
     logo.setAttribute('alt', `${buisness.name} Logo`);
@@ -101,6 +101,7 @@ function displayBuisnesses(buisness) {
     h2.innerText = `${buisness.address} 
     ${buisness.phone}`;
 
+    link.setAttribute('href', buisness.website)
     link.innerText = buisness.website
 
 
